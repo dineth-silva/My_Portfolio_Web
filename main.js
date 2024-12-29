18489f3ae6f51a8cd42f53f750040f13ca29c83f
dineth-silva <dinethsilva6677@gmail.com>
2024-12-29 11:12:02 +0530

    typed js

--- main.js
+++ main.js
@@ -40,3 +40,29 @@ window.onscroll = () => {
 
 
 }
+
+/* ================ scroll reveal=====================   */
+ScrollReveal({
+    distance:'80px',
+    duration:2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .service-container,.portfolio-box,.contact form', { origin:'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/* ================ typed js=====================   */
+const element =  document.getElementById("multiple-text");
+setInterval(function() {element.innerHTML += "Hello"}, 1000);
+
+
+
+/*new Typed('.multiple-text',{strings: ['Frontend Developer','Web Designer','Youtuber'],
+    typeSpeed: 70
+    ,backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+})*/;
